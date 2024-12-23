3c71fc7adfdef62a233c8084ced8ba255b4bd6a1
glypher <matmih2002@yahoo.com>
2024-12-23 20:51:56 +0200

    Fixed tracing endpoint

--- spring-bit-crypto/src/main/java/com/springbit/crypto/configuration/TracingConfig.java
+++ spring-bit-crypto/src/main/java/com/springbit/crypto/configuration/TracingConfig.java
@@ -1,14 +1,27 @@
 package com.springbit.crypto.configuration;
 
+import io.micrometer.observation.Observation;
+import io.micrometer.observation.ObservationHandler;
 import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.observation.ObservationTextPublisher;
 import io.micrometer.observation.aop.ObservedAspect;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TracingConfig {
+    private static final Logger logger = LoggerFactory.getLogger(TracingConfig.class);
+
     @Bean
     ObservedAspect observedAspect(ObservationRegistry observationRegistry) {
         return new ObservedAspect(observationRegistry);
     }
+
+    @Bean
+    public ObservationHandler<Observation.Context> observationTextPublisher() {
+        return new ObservationTextPublisher(logger::info);
+    }
+
 }
