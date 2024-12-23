b379ae561a51103ac825347dcb1ad931f6de6cd0
glypher <matmih2002@yahoo.com>
2024-12-23 16:27:36 +0200

    Add grafana/loki support and gafana log view

--- spring-bit-crypto/src/test/java/com/springbit/crypto/SpringBitCryptoApplicationTests.java
+++ spring-bit-crypto/src/test/java/com/springbit/crypto/SpringBitCryptoApplicationTests.java
@@ -2,8 +2,10 @@ package com.springbit.crypto;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpringBitCryptoApplicationTests {
 
     @Test
