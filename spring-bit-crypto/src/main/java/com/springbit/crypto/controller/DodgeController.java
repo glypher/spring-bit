7e5b11482d7cd2fd97153684af134b17f5e672a3
glypher <matmih2002@yahoo.com>
2024-12-24 14:21:02 +0200

    Added API gateway with spring cloud gateway

--- spring-bit-crypto/src/main/java/com/springbit/crypto/controller/DodgeController.java
+++ spring-bit-crypto/src/main/java/com/springbit/crypto/controller/DodgeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
 
 @RestController
-@RequestMapping("/api/dodge")
+@RequestMapping("/crypto/dodge")
 @RequiredArgsConstructor
 public class DodgeController {
     private final EntMapper modelMapper;
---
+++ spring-bit-gateway/src/main/java/com/springbit/gateway/SpringBitGatewayApplication.java
@@ -0,0 +1,15 @@
+package com.springbit.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class SpringBitGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBitGatewayApplication.class, args);
+    }
+
+}
